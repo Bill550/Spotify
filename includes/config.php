4d6597e20d689363a890a8c10fd1697fd7e50947
Muhammad Bilal <bilalsuhail@rocketmail.com>
2020-03-10 20:03:40 +0500

    4th Changes

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
     session_start();
     
     $timezone = date_default_timezone_set ("Asia/Karachi");
-    $con = mysqli_connect("Localhost", "root", "", "Spotify");
+    $con = mysqli_connect('Localhost', 'root', '', 'Spotify');
     if(mysqli_connect_errno()){
         echo "Failed To Connect: ". mysqli_connect_errno();
     }
